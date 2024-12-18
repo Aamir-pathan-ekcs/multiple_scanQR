41fc3af1ee4d367be30aa3bd22ba35fb2c07a5e8
Aamir-pathan-ekcs <maakhan@ekcs.co>
2024-12-18 22:52:27 +0530

    scan qr updated

--- app.js
+++ app.js
@@ -60,9 +60,7 @@ io.on("connection", (socket) => {
   });
   socket.on("scan-qr", (data) => {
     console.log("QR code scanned:", data);
-    alert('testMessage');
-    // Perform an action, e.g., broadcast to other clients or a specific room
-    io.emit("qr-scanned", { message: "QR code scanned successfully", data });
+    io.to(data.sessionId).emit("qr-scanned", { message: "QR code scanned successfully" });
   });
 });
 
